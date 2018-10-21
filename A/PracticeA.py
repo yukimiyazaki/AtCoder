388a50148060b1f57f0bc5b8e70def2257db885c
yukimiyazaki <u778358j@ecs.osaka-u.ac.jp>
2018-10-21 13:33:07 +0900

    Create PracticeA.py

---
+++ A/PracticeA.py
@@ -0,0 +1,11 @@
+PracticeA - はじめてのあっとこーだー（Welcome to AtCoder）
+ポイント：入力方法と出力方法を学ぶ
+# -*- coding: utf-8 -*-
+# 整数の入力
+a = int(input())
+# スペース区切りの整数の入力
+b, c = map(int, input().split())
+# 文字列の入力
+s = input()
+# 出力
+print("{} {}".format(a+b+c, s))
